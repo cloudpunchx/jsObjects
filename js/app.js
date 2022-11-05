a46099c71cdc2c7e4ba60b156c3985154899c552
cloudpunchx <siobhanbonardi@hotmail.com>
2022-11-05 13:52:13 -0600

    Completed small knowledge check working with Objects for the first time.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let company = {
+    name: `Cloudpunch`,
+    yearlyRevenue: 500000000,
+    ceo: `Siobhan Bonardi`,
+    numberOfEmployees: 10,
+};
+
+if (company[`yearlyRevenue`] >= 1000000){
+    console.log("Congrats");
+    console.log(company[`name`], company[`ceo`], company[`numberOfEmployees`]);
+} else{
+    console.log(`Keep going Cloudpunch, you can do it`);
+}
